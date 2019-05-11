c5ed4f1937820d2fe426d83aa94fc15d6be91c5d
yuhao <yuhao819@hotmail.com>
2019-05-12 02:21:44 +0800

    Update pyIRIS.py

--- bin/pyIRIS.py
+++ bin/pyIRIS.py
@@ -226,7 +226,7 @@ class Detector:
         blob_params = cv.SimpleBlobDetector_Params()
 
         blob_params.minThreshold = 1
-        blob_params.thresholdStep = 2
+        blob_params.thresholdStep = 1
         blob_params.minRepeatability = 2
         blob_params.minDistBetweenBlobs = 1
 
