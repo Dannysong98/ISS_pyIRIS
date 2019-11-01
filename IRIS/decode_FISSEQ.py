f3c4c5f2a3c31c3fef5d6ce08a9f8c1195b081f1
yuhao <yuhao819@hotmail.com>
2019-11-01 14:26:50 +0800

    update decode_FISSEQ.py

--- IRIS/decode_FISSEQ.py
+++ IRIS/decode_FISSEQ.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-This model is used to decode the result of Ke into FISSEQ.
+This model is used to decode the pseudo-result (Ke's data-like) into result of FISSEQ.
 """
+
 
 from numpy import (array,
                    uint8)
@@ -25,9 +26,9 @@ class graph:
                    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1]], dtype=uint8)
 
-    M_lab_coor = {'AA': 0, 'AT': 1, 'AC': 2, 'AG': 3,
-                  'TA': 4, 'TT': 5, 'TC': 6, 'TG': 7,
-                  'CA': 8, 'CT': 9, 'CC': 10, 'CG': 11,
+    M_lab_coor = {'AA': 0,  'AT': 1,  'AC': 2,  'AG': 3,
+                  'TA': 4,  'TT': 5,  'TC': 6,  'TG': 7,
+                  'CA': 8,  'CT': 9,  'CC': 10, 'CG': 11,
                   'GA': 12, 'GT': 13, 'GC': 14, 'GG': 15}
 
     code = {'A': ['AA', 'TT', 'CC', 'GG'],
@@ -37,9 +38,6 @@ class graph:
 
     def __init__(self):
         """"""
-        self.p_node = ''
-        self.n_node = ''
-
         self.score = 0
         self.decoded_seq = ''
 
@@ -48,11 +46,11 @@ class graph:
         self.decoded_seq = seq
 
         for sig in ext:
-            for pair in graph.code[sig]:
-                self.score = graph.P_map[graph.M_lab_coor[self.decoded_seq[-2:]], graph.M_lab_coor[pair]]
+            for new_node in graph.code[sig]:
+                self.score = graph.P_map[graph.M_lab_coor[self.decoded_seq[-2:]], graph.M_lab_coor[new_node]]
 
                 if self.score == 1:
-                    self.decoded_seq += pair[-1]
+                    self.decoded_seq += new_node[-1]
                     break
 
 
